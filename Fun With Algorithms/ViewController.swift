16fdfe2d3c889bb0b1a6d02237704967f642713f
rammhapasekar <mhapasekar.ram@gmail.com>
2017-04-15 12:16:29 +0530

    Initial Commit

---
+++ Fun With Algorithms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun With Algorithms
+//
+//  Created by Ram Mhapasekar on 15/04/17.
+//  Copyright © 2017 rammhapasekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
